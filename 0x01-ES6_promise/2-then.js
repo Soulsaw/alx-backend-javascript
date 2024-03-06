d3ef2280995bb55c4f973d4d120e5fbab8b01edc
Soulsaw <ssawadogo292@gmail.com>
2024-03-06 23:52:16 +0000

    fix task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise
+  promise
     .then(() => ({ status: 200, body: 'Success' }))
     .catch(() => (new Error()))
     .finally(console.log('Got a response from the API'));
