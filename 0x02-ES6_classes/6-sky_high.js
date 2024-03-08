4359667fbb00ecf4fa910cb8f680731c99052ea7
Soulsaw <ssawadogo292@gmail.com>
2024-03-08 03:25:43 +0000

    fix

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,5 +1,24 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  constructor(sqft, floors)
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if (typeof (floors) === 'number') {
+      this._floors = floors;
+    } else {
+      throw new TypeError('Floor must be a number');
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
 }
