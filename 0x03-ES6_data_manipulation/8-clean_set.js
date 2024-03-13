130f12ee0e3818322d1d1b1a91dc1e7b061dd667
Soulsaw <ssawadogo292@gmail.com>
2024-03-13 08:40:34 +0000

    fix task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
-export default function cleanSet(set, startString) {
+export default function cleanSet(set, startString = '') {
+  if (startString.length === 0) {
+    return '';
+  }
   return Array.from(set)
     .filter(((value) => value.startsWith(startString)))
     .map((value) => value.slice(startString.length))
