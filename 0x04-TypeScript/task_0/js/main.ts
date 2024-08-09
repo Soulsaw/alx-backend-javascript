1f054f162e89f4f0934dc2e17c7725f898645e19
Soulsaw <ssawadogo292@gmail.com>
2024-08-09 11:01:18 +0000

    add the file gitignore

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,3 +20,26 @@ const user2: Student = {
   };
 
 const studentsList: Student[] = [user1, user2];
+
+function renderTable(students: Student[]): void {
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+
+  students.forEach(student => {
+    const row = document.createElement('tr');
+    const firstNameCell = document.createElement('td');
+    const locationCell = document.createElement('td');
+
+    firstNameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+
+    row.appendChild(firstNameCell);
+    row.appendChild(locationCell);
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+  document.body.appendChild(table);
+}
+
+renderTable(studentsList);
