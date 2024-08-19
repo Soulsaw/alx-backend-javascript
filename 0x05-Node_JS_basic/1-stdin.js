6e000507ab3a734bd283fafda5645848aca86916
Soulsaw <ssawadogo292@gmail.com>
2024-08-19 09:36:46 +0000

    fix the task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+const process = require("process");
+const displayMessage = require("./0-console");
+
+displayMessage("Welcome to Holberton School, what is your name?");
+process.stdin.on("readable", () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name: ${name}`);
+  }
+});
+
+process.stdin.on("end", () => {
+  displayMessage(`This important software is now closing`);
+});
