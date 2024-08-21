c41b86ed41a59db95196a9e27141f349eccd7b91
Soulsaw <ssawadogo292@gmail.com>
2024-08-21 10:31:50 +0000

    fix the unittest for 1-calcul,js

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('culculateNumber', () => {
+  describe('#SUM', () => {
+    it('...', () => {
+      assert.equal(calculateNumber('SUM', 1, 3), 4);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUM', 1, 3.7), 5);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUM', 1.5, 3.7), 6);
+    });
+  });
+  describe('#SUBTRACK', () => {
+    it('...', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1, 3.7), -3);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUBTRACT', -1.2, -3.7), 3);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('SUBTRACT', 1.5, -3.7), 6);
+    });
+  });
+  describe('#DIVIDE', () => {
+    it('...', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+    it('...', () => {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+});
