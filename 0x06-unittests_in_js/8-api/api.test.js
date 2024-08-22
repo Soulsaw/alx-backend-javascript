c597e7afbd8e3db5afd1d6a2e569bdfcbaf3aedf
Soulsaw <ssawadogo292@gmail.com>
2024-08-22 22:57:18 +0000

    fix the api

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('Test the api', () => {
+    it('#Status code is equal to 200', () => {
+        request.get('http://localhost:7865', (err, res, body) => {
+            expect(res.statusCode).to.be.equal(200);
+        });
+    });
+    it("#Correct result is 'Welcome to the payment system'", () => {
+        request.get('http://localhost:7865', (err, res, body) => {
+            expect(res.body).to.be.equal('Welcome to the payment system');
+        });
+    });
+});
